eae38ce282e071f12efde0c9aaef7bee0bac3e9d
Kostadin Dinkov <carobant@gmail.com>
2019-12-06 15:48:10 +0200

    Fixed a bug in date? function and added function 4

--- Homework/HW1/Solution-3.rkt
+++ Homework/HW1/Solution-3.rkt
@@ -25,23 +25,32 @@
       (= month 12)))
 
 (define (date? date)
-  (cond ((= (month date) 2) (if (leap? (year date))
-                                (and (< (day date) 30)
-                                     (> (day date) 0))
-                                (and (< (day date) 29)
-                                     (> (day date) 0))))
-        ((long-month? (month date)) (if (and (< (day date) 32)
-                                             (> (day date) 0))
-                                        #t
-                                        #f))
-        (else (if (and (< (day date) 31)
-                       (> (day date) 0))
-                   #t
-                   #f))))
+  (if (or (< (month date) 1) (> (month date) 12))
+      #f
+      (cond ((= (month date) 2) (if (leap? (year date))
+                                    (and (< (day date) 30)
+                                         (> (day date) 0))
+                                    (and (< (day date) 29)
+                                         (> (day date) 0))))
+            ((long-month? (month date)) (if (and (< (day date) 32)
+                                                 (> (day date) 0))
+                                            #t
+                                            #f))
+            (else (if (and (< (day date) 31)
+                           (> (day date) 0))
+                      #t
+                      #f)))))
 
 (define (date->string date)
   (string-append (number->string (car date))
                  "."
                  (number->string (cadr date))
                  "."
-                 (number->string (caddr date)))) 
+                 (number->string (caddr date))))
+
+(define (next-day date)
+  (if (date? (cons (+ (day date) 1) (cdr date)))
+      (cons (+ (day date) 1) (cdr date))
+      (if (date? (list 1 (+ (month date) 1) (year date)))
+          (list 1 (+ (month date) 1) (year date))
+          (list 1 1 (+ (year date) 1)))))
